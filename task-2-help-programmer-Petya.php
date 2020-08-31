17fe5bd1cba5c19da9be821b9ab13714f1342fee
Evgeniy Kochetkov <ekochetkov.work@gmail.com>
2020-08-31 22:43:26 +0300

    Доработано описание.

--- task-2-help-programmer-Petya.php
+++ task-2-help-programmer-Petya.php
@@ -1,41 +1,5 @@
 <?php
 
-
-function minCommits($f,$w){
-
-    if( $w == 0 ){
-
-        if( $f == 0 )
-            return 0;
-
-        if( $f % 2 == 1)
-            return -1;
-        else
-            return $f / 2;
-
-    }
-
-    $count = 0;
-
-    if ( $w % 2 == 1 )
-        {
-            $count++;
-            $w += 1;
-        }
-
-    $count += $w / 2;
-
-    $f = $f + $w / 2;
-    
-    $count += intval($f / 2);
-
-    if( $f % 2 == 1 )
-        $count += 4;
-
-    return $count;
-
-}
-
 function getMinPathFromPoint($f,$w,&$map){
 
     ini_set('memory_limit', '-1');
@@ -72,7 +36,7 @@ function getMinPathFromPoint($f,$w,&$map){
 
 }
 
-function minCommitsV2($f,$w){
+function minCommits($f,$w){
 
     $map = [];
 
@@ -81,3 +45,39 @@ function minCommitsV2($f,$w){
     return $map[$f][$w]>=1000000?-1:$map[$f][$w];
 
 }
+
+function minCommitsV2($f,$w){
+
+    if( $w == 0 ){
+
+        if( $f == 0 )
+            return 0;
+
+        if( $f % 2 == 1)
+            return -1;
+        else
+            return $f / 2;
+
+    }
+
+    $count = 0;
+
+    if ( $w % 2 == 1 )
+        {
+            $count++;
+            $w += 1;
+        }
+
+    $count += $w / 2;
+
+    $f = $f + $w / 2;
+    
+    $count += intval($f / 2);
+
+    if( $f % 2 == 1 )
+        $count += 4;
+
+    return $count;
+
+}
+
